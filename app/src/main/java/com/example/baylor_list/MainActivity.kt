92b4cb7611fe62f948c2120773c47255fde36323
Price, Juan <juan.price@one.verizon.com>
2019-06-05 12:18:07 -0500

    Added Network Check

--- app/src/main/java/com/example/baylor_list/MainActivity.kt
+++ app/src/main/java/com/example/baylor_list/MainActivity.kt
@@ -15,18 +15,29 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import android.R.attr.orientation
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
-
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+                if(NetworkAvailable()) {
+                    FetchUsers()
+                }else{
 
-        FetchUsers()
+                  Toast.makeText(applicationContext,"Network Error",Toast.LENGTH_SHORT).show()
+                }
 
 
     }
@@ -58,6 +69,17 @@ class MainActivity : AppCompatActivity() {
 
 
         })
+    }
+
+    private fun NetworkAvailable():Boolean{
+
+
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+
+        return isConnected
+
     }
 
     override fun onResume() {
--- app/src/main/java/com/example/baylor_list/detail.kt
+++ app/src/main/java/com/example/baylor_list/detail.kt
@@ -27,7 +27,7 @@ class detail : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            .setAction("Action", null).show()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
